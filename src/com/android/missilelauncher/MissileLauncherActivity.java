31ce385e9fd8c7d8d93b7283e80a30b149a62607
Falko Richter <falko.richter@sinnerschrader-mobile.de>
2012-12-17 09:04:23 +0100

    this is the original sample code

--- src/com/android/missilelauncher/MissileLauncherActivity.java
+++ src/com/android/missilelauncher/MissileLauncherActivity.java
@@ -34,294 +34,216 @@ import android.hardware.usb.UsbManager;
 import android.hardware.usb.UsbRequest;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnTouchListener;
 import android.widget.Button;
 
 public class MissileLauncherActivity extends Activity
-implements View.OnClickListener, Runnable, OnTouchListener {
+        implements View.OnClickListener, Runnable {
 
-	private static final String TAG = "MissileLauncherActivity";
+    private static final String TAG = "MissileLauncherActivity";
 
-	private Button mMoveUp;
-	private Button mMoveLeft;
-	private Button mMoveRight;
-	private Button mMoveDown;
-	private Button mFireButton2;
+    private Button mFire;
+    private UsbManager mUsbManager;
+    private UsbDevice mDevice;
+    private UsbDeviceConnection mConnection;
+    private UsbEndpoint mEndpointIntr;
+    private SensorManager mSensorManager;
+    private Sensor mGravitySensor;
 
-	private UsbManager mUsbManager;
-	private UsbDevice mDevice;
-	private UsbDeviceConnection mConnection;
-	private UsbEndpoint mEndpointIntr;
-	private SensorManager mSensorManager;
-	private Sensor mGravitySensor;
+    // USB control commands
+    private static final int COMMAND_UP = 1;
+    private static final int COMMAND_DOWN = 2;
+    private static final int COMMAND_RIGHT = 4;
+    private static final int COMMAND_LEFT = 8;
+    private static final int COMMAND_FIRE = 16;
+    private static final int COMMAND_STOP = 32;
+    private static final int COMMAND_STATUS = 64;
 
-	// USB control commands
-	private static final int COMMAND_UP = 1;
-	private static final int COMMAND_DOWN = 2;
-	private static final int COMMAND_RIGHT = 4;
-	private static final int COMMAND_LEFT = 8;
-	private static final int COMMAND_FIRE = 10;
-	private static final int COMMAND_STOP = 20;
-	private static final int COMMAND_STATUS = 64;
+    // constants for accelerometer orientation
+    private static final int TILT_LEFT = 1;
+    private static final int TILT_RIGHT = 2;
+    private static final int TILT_UP = 4;
+    private static final int TILT_DOWN = 8;
+    private static final double THRESHOLD = 5.0;
 
-	// constants for accelerometer orientation
-	private static final int TILT_LEFT = 1;
-	private static final int TILT_RIGHT = 2;
-	private static final int TILT_UP = 8;
-	private static final int TILT_DOWN = 4;
-	private static final double THRESHOLD = 5.0;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+        setContentView(R.layout.launcher);
+        mFire = (Button)findViewById(R.id.fire);
+        mFire.setOnClickListener(this);
 
-		setContentView(R.layout.launcher);
+        mUsbManager = (UsbManager)getSystemService(Context.USB_SERVICE);
 
-		mMoveUp = (Button)findViewById(R.id.moveUp);
-		mMoveUp.setOnClickListener(this);
-		mMoveUp.setOnTouchListener(this);
+        mSensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        mGravitySensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+    }
 
-		mMoveLeft = (Button)findViewById(R.id.moveLeft);
-		mMoveLeft.setOnClickListener(this);
-		mMoveLeft.setOnTouchListener(this);
-		
-		mMoveRight = (Button)findViewById(R.id.moveRight);
-		mMoveRight.setOnClickListener(this);
-		mMoveRight.setOnTouchListener(this);
-		
-		mMoveDown = (Button)findViewById(R.id.moveDown);
-		mMoveDown.setOnClickListener(this);
-		mMoveDown.setOnTouchListener(this);
+    @Override
+    public void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(mGravityListener);
+    }
 
-		mFireButton2 = (Button)findViewById(R.id.fire2);
-		mFireButton2.setOnClickListener(this);
+    @Override
+    public void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(mGravityListener, mGravitySensor,
+                SensorManager.SENSOR_DELAY_NORMAL);
 
-		mUsbManager = (UsbManager)getSystemService(Context.USB_SERVICE);
+        Intent intent = getIntent();
+        Log.d(TAG, "intent: " + intent);
+        String action = intent.getAction();
 
-		mSensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
-		mGravitySensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
-	}
+        UsbDevice device = (UsbDevice)intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+        if (UsbManager.ACTION_USB_DEVICE_ATTACHED.equals(action)) {
+            setDevice(device);
+        } else if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
+            if (mDevice != null && mDevice.equals(device)) {
+                setDevice(null);
+            }
+        }
+    }
 
-	@Override
-	public void onPause() {
-		super.onPause();
-		mSensorManager.unregisterListener(mGravityListener);
-	}
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
 
-	@Override
-	public void onResume() {
-		super.onResume();
-		mSensorManager.registerListener(mGravityListener, mGravitySensor,
-				SensorManager.SENSOR_DELAY_NORMAL);
+    private void setDevice(UsbDevice device) {
+        Log.d(TAG, "setDevice " + device);
+        if (device.getInterfaceCount() != 1) {
+            Log.e(TAG, "could not find interface");
+            return;
+        }
+        UsbInterface intf = device.getInterface(0);
+        // device should have one endpoint
+        if (intf.getEndpointCount() != 1) {
+            Log.e(TAG, "could not find endpoint");
+            return;
+        }
+        // endpoint should be of type interrupt
+        UsbEndpoint ep = intf.getEndpoint(0);
+        if (ep.getType() != UsbConstants.USB_ENDPOINT_XFER_INT) {
+            Log.e(TAG, "endpoint is not interrupt type");
+            return;
+        }
+        mDevice = device;
+        mEndpointIntr = ep;
+        if (device != null) {
+            UsbDeviceConnection connection = mUsbManager.openDevice(device);
+            if (connection != null && connection.claimInterface(intf, true)) {
+                Log.d(TAG, "open SUCCESS");
+                mConnection = connection;
+                Thread thread = new Thread(this);
+                thread.start();
 
-		Intent intent = getIntent();
-		Log.d(TAG, "intent: " + intent);
-		String action = intent.getAction();
+            } else {
+                Log.d(TAG, "open FAIL");
+                mConnection = null;
+            }
+         }
+    }
 
-		UsbDevice device = (UsbDevice)intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
-		if (UsbManager.ACTION_USB_DEVICE_ATTACHED.equals(action)) {
-			setDevice(device);
-		} else if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
-			if (mDevice != null && mDevice.equals(device)) {
-				setDevice(null);
-			}
-		}
-	}
+    private void sendCommand(int control) {
+        synchronized (this) {
+            if (control != COMMAND_STATUS) {
+                Log.d(TAG, "sendMove " + control);
+            }
+            if (mConnection != null) {
+                byte[] message = new byte[1];
+                message[0] = (byte)control;
+                // Send command via a control request on endpoint zero
+                mConnection.controlTransfer(0x21, 0x9, 0x200, 0, message, message.length, 0);
+            }
+        }
+    }
 
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-	}
+    public void onClick(View v) {
+        if (v == mFire) {
+            sendCommand(COMMAND_FIRE);
+        }
+    }
 
-	private void setDevice(UsbDevice device) {
-		Log.d(TAG, "setDevice " + device);
-		if (device.getInterfaceCount() != 1) {
-			Log.e(TAG, "could not find interface");
-			return;
-		}
-		UsbInterface intf = device.getInterface(0);
-		// device should have one endpoint
-		if (intf.getEndpointCount() != 1) {
-			Log.e(TAG, "could not find endpoint");
-			return;
-		}
-		// endpoint should be of type interrupt
-		UsbEndpoint ep = intf.getEndpoint(0);
-		if (ep.getType() != UsbConstants.USB_ENDPOINT_XFER_INT) {
-			Log.e(TAG, "endpoint is not interrupt type");
-			return;
-		}
-		mDevice = device;
-		mEndpointIntr = ep;
-		if (device != null) {
-			UsbDeviceConnection connection = mUsbManager.openDevice(device);
-			if (connection != null && connection.claimInterface(intf, true)) {
-				Log.d(TAG, "open SUCCESS");
-				mConnection = connection;
-				Thread thread = new Thread(this);
-				thread.start();
+    private int mLastValue = 0;
 
-			} else {
-				Log.d(TAG, "open FAIL");
-				mConnection = null;
-			}
-		}
-	}
+    SensorEventListener mGravityListener = new SensorEventListener() {
+        public void onSensorChanged(SensorEvent event) {
 
-	private void sendCommand(int control) {
-		if (mConnection != null) {
-			synchronized (this) {
+            // compute current tilt
+            int value = 0;
+            if (event.values[0] < -THRESHOLD) {
+                value += TILT_LEFT;
+            } else if (event.values[0] > THRESHOLD) {
+                value += TILT_RIGHT;
+            }
+            if (event.values[1] < -THRESHOLD) {
+                value += TILT_UP;
+            } else if (event.values[1] > THRESHOLD) {
+                value += TILT_DOWN;
+            }
 
-				Log.d(TAG, "sendMove " + control);
-				byte[] message = new byte[8];
-				message[0] = 0x2;
-				message[2] = 0;
-				message[3] = 0;
-				message[4] = 0;
-				message[5] = 0;
-				message[6] = 0;
-				message[7] = 0;
-				switch (control) {
-				case COMMAND_DOWN:
-					message[1] = 0x1;
-					break;
-				case COMMAND_LEFT:
-					message[1] = 0x4;
-					break;
-				case COMMAND_UP:
-					message[1] = 0x2;
-					break;
-				case COMMAND_RIGHT:
-					message[1] = 0x8;
-					break;
-				case COMMAND_FIRE:
-					message[1] = 0x10;
-					break;
-				case COMMAND_STOP:
-					message[1] = 0x20;
-					break;
-				default:
+            if (value != mLastValue) {
+                mLastValue = value;
+                // send motion command if the tilt changed
+                switch (value) {
+                    case TILT_LEFT:
+                        sendCommand(COMMAND_LEFT);
+                        break;
+                    case TILT_RIGHT:
+                       sendCommand(COMMAND_RIGHT);
+                        break;
+                    case TILT_UP:
+                        sendCommand(COMMAND_UP);
+                        break;
+                    case TILT_DOWN:
+                        sendCommand(COMMAND_DOWN);
+                        break;
+                    default:
+                        sendCommand(COMMAND_STOP);
+                        break;
+                }
+            }
+        }
 
-				}
-				mConnection.controlTransfer(0x21, 0x9, 0x200, 0, message, message.length, 0);
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // ignore
+        }
+    };
 
-			}
-		}
-		else{
-			Log.d(TAG, "don«t sendMove " + control);
-		}
-	}
-
-	public void onClick(View v) {
-		
-	}
-
-	private int mLastValue = 0;
-
-	SensorEventListener mGravityListener = new SensorEventListener() {
-		public void onSensorChanged(SensorEvent event) {
-
-			// compute current tilt
-			int value = 0;
-			if (event.values[0] < -THRESHOLD) {
-				value += TILT_LEFT;
-			} else if (event.values[0] > THRESHOLD) {
-				value += TILT_RIGHT;
-			}
-			if (event.values[1] < -THRESHOLD) {
-				value += TILT_UP;
-			} else if (event.values[1] > THRESHOLD) {
-				value += TILT_DOWN;
-			}
-
-			if (value != mLastValue) {
-				mLastValue = value;
-				// send motion command if the tilt changed
-				switch (value) {
-				case TILT_LEFT:
-					sendCommand(COMMAND_RIGHT);
-					break;
-				case TILT_RIGHT:
-					sendCommand(COMMAND_LEFT);
-					break;
-				case TILT_UP:
-					sendCommand(COMMAND_DOWN);
-					break;
-				case TILT_DOWN:
-					sendCommand(COMMAND_UP);
-					break;
-				default:
-					sendCommand(COMMAND_STOP);
-					break;
-				}
-			}
-		}
-
-		public void onAccuracyChanged(Sensor sensor, int accuracy) {
-			// ignore
-		}
-	};
-
-	@Override
-	public void run() {
-		ByteBuffer buffer = ByteBuffer.allocate(1);
-		UsbRequest request = new UsbRequest();
-		request.initialize(mConnection, mEndpointIntr);
-		byte status = -1;
-		while (true) {
-			// queue a request on the interrupt endpoint
-			request.queue(buffer, 1);
-			// send poll status command
-			sendCommand(COMMAND_STATUS);
-			// wait for status event
-			if (mConnection.requestWait() == request) {
-				byte newStatus = buffer.get(0);
-				Log.d(TAG, "got newStatus " + newStatus);
-				if (newStatus != status) {
-					Log.d(TAG, "got status " + newStatus);
-					status = newStatus;
-					if ((status & COMMAND_FIRE) != 0) {
-						// stop firing
-						sendCommand(COMMAND_STOP);
-					}
-				}
-				try {
-					Thread.sleep(100);
-				} catch (InterruptedException e) {
-				}
-			} else {
-				Log.e(TAG, "requestWait failed, exiting");
-				break;
-			}
-		}
-	}
-
-	@Override
-	public boolean onTouch(View v, MotionEvent event) {
-		if(event.getAction() == MotionEvent.ACTION_DOWN){
-			if (v == mMoveDown) {
-				sendCommand(COMMAND_DOWN);
-			}
-			else if (v == mMoveLeft) {
-				sendCommand(COMMAND_LEFT);
-			}
-			else if (v == mMoveRight) {
-				sendCommand(COMMAND_RIGHT);
-			}
-			else if (v == mMoveUp) {
-				sendCommand(COMMAND_UP);
-			}
-			else if (v == mFireButton2) {
-				sendCommand(COMMAND_FIRE);
-			}
-		}
-		else if(event.getAction() == MotionEvent.ACTION_UP){
-			sendCommand(COMMAND_STOP);
-		}
-		return false;
-	}
+    @Override
+    public void run() {
+        ByteBuffer buffer = ByteBuffer.allocate(1);
+        UsbRequest request = new UsbRequest();
+        request.initialize(mConnection, mEndpointIntr);
+        byte status = -1;
+        while (true) {
+            // queue a request on the interrupt endpoint
+            request.queue(buffer, 1);
+            // send poll status command
+            sendCommand(COMMAND_STATUS);
+            // wait for status event
+            if (mConnection.requestWait() == request) {
+                byte newStatus = buffer.get(0);
+                if (newStatus != status) {
+                    Log.d(TAG, "got status " + newStatus);
+                    status = newStatus;
+                    if ((status & COMMAND_FIRE) != 0) {
+                        // stop firing
+                        sendCommand(COMMAND_STOP);
+                    }
+                }
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                }
+            } else {
+                Log.e(TAG, "requestWait failed, exiting");
+                break;
+            }
+        }
+    }
 }
 
 
